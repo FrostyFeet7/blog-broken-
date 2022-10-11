821895325cfbea29c3654f1f869373adc057b007
FrostyFeet7 <tyzer.clark@umconnect.umt.edu>
2022-10-11 11:15:42 -0600

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView  # new
+from .views import (
+    BlogListView, 
+    BlogDetailView, 
+    BlogCreateView,
+    BlogUpdateView,
+)  # new
 
 urlpatterns = [
     path("path/new/", BlogCreateView.as_view(), name="post_new"),
-    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),  # new
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("path/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),  # new
     path("", BlogListView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 
@@ -16,4 +16,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post__new.html"
     fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Posttemplate_name = "post_edit.html"
+    fields =["title", "body"]
 # Create your views here.
